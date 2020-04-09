de80b407ede2bd9b31a351f6a857598f7df9f5c8
cotoflux <guaupelu@hotmail.com>
2020-04-09 11:44:49 +0200

    Inicio ejercicio fase 4

---
+++ Fase4/main.js
@@ -0,0 +1,23 @@
+
+function calcular(){
+
+    var any1948 = 1948;
+    var cadaQuant = 4;
+    const naixement = 1990;
+    var anyTraspas = any1948 + cadaQuant;
+    var diferencia = naixement - any1948;
+    const loop = Math.floor(diferencia / cadaQuant);
+
+    for(i=0; i<loop ; i++){
+        anyTraspas = anyTraspas + cadaQuant;
+        
+        if(anyTraspas<1990){
+            var mensaje1 = anyTraspas + "Aquest es un any de traspàs <br>";
+            document.getElementById("dades").innerHTML += mensaje1;
+        } else {
+            var mensaje2 = "Aquest no es un any de traspàs <br>";
+            document.getElementById("dades").innerHTML = mensaje2;
+        }
+    }
+   
+}
