2ef6c38d45385671eea510db8209c8a43bd41c0c
cotoflux <guaupelu@hotmail.com>
2020-04-09 11:43:48 +0200

    Finalizado ejercicio 3

--- Fase3/main.js
+++ Fase3/main.js
@@ -1,13 +1,23 @@
 
 function calcular(){
 
-    const any1948 = 1948;
+    var any1948 = 1948;
     var cadaQuant = 4;
     const naixement = 1990;
+    var anyTraspas = any1948 + cadaQuant;
+    var diferencia = naixement - any1948;
+    const loop = Math.floor(diferencia / cadaQuant);
 
-    var diferencia = (naixement - any1948);
-    var quantsAnys = diferencia / cadaQuant;
+    for(i=0; i<loop ; i++){
+        anyTraspas = anyTraspas + cadaQuant;
+        
+        if(anyTraspas<1990){
+            var mensaje1 = anyTraspas + "Aquest es un any de traspàs <br>";
+            document.getElementById("dades").innerHTML += mensaje1;
+        } else {
+            var mensaje2 = "Aquest no es un any de traspàs <br>";
+            document.getElementById("dades").innerHTML = mensaje2;
+        }
+    }
    
-    document.getElementById("dades").innerHTML = Math.floor(quantsAnys);
-
 }
